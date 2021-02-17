af35391bbd7618cfefc39492a222a68d74e41ccf
Maciej Lewkowicz <lewkowicz@tuta.io>
2021-02-17 05:43:13 +0100

    Redesigned error generation

--- main.java
+++ main.java
@@ -1,32 +1,57 @@
 package correcter;
 import java.util.*;
 
-//Class Encoder {
-//    
-//   int    seed;
-//   String encodingType;
-//  
-//    public Cezar(String encodingType, int seed){
-//        
-//    }
-//}
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        String message = scanner.nextLine();
-        char[] encryptedMessage = message.toCharArray();
-
-	// Simple encoding [Moving to class in future]
-        for (int i = 0; i < encryptedMessage.length; i++) {
-            int k = random.nextInt(5) + 1; //Seed per loop iteration.
-            if (i % 3 == 0) {
-                encryptedMessage[i] += k; //Switching symbol.
+        char randomLetter;
+        String firstLetter, secondLetter, thirdLetter;
+        String inputText = scanner.nextLine();
+        String[] message = inputText.split("");
+        List<String> encryptedMessage = new ArrayList<String>(); 
+        
+        for (String str : message) {
+            System.out.print(str);
+        }
+        System.out.println("");
+        
+        // Tripling and creating one error per three letters.
+        for (int i = 0; i < message.length; i++) {
+            int error = random.nextInt(2);
+            for (int j = 0; j <= 2; j++) {
+            System.out.print(message[i]);
+            if (j == error){
+                
+                    // Checking if the error letter is not duplicated.
+                    do {
+                        randomLetter = (char) ('a' + random.nextInt(26));
+                    } while (message[i].equals(Character.toString(randomLetter))); // If yes repeat
+                    
+                    encryptedMessage.add(Character.toString(randomLetter));
+                } else {
+                    encryptedMessage.add(message[i]);
+                }
             }
         }
-
-        System.out.println(encryptedMessage);
+        
+        System.out.println("");
+        for (String str : encryptedMessage) {
+        System.out.print(str);
+        }
+        System.out.println("");
+        
+        // Decoding by comparing 2 letter per triplet
+        for (int i = 0; i < encryptedMessage.size(); i+=3) {
+            firstLetter = encryptedMessage.get(i);
+            secondLetter = encryptedMessage.get(i + 1);
+            thirdLetter = encryptedMessage.get(i + 2);
+            if (thirdLetter.equals(firstLetter)) {
+                System.out.print(firstLetter);
+            } else {
+                System.out.print(secondLetter);
+            }
+        }
     }
 }
